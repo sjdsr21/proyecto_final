546cc6a2754bb2a2192d98f3352dfddd52188886
sjdsr21 <desousasanti@gmail.com>
2023-01-09 12:56:32 -0400

    add

--- pruebas.R
+++ pruebas.R
@@ -1,1 +1,12 @@
-a <- 'b'
+library(dslabs)
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(ggthemes)
+library(gridExtra)
+
+crimen <- read_excel("crimen_violento.xlsx")
+
+estados <- crimen$Year[14:64]
+
+crimen_estado <- filter(crimen, `Year` %in% estados)
